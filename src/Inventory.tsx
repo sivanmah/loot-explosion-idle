a089f289d1e107e780e29c3537a5b6745acc3b14
sivanmah <sivanmahmud@gmail.com>
2025-06-04 12:45:26 +0200

    inventory grid design

--- src/Inventory.tsx
+++ src/Inventory.tsx
@@ -1,10 +1,21 @@
+import InventoryItem from "./InventoryItem";
 import type { CurrencyState } from "./types";
 
 export default function Inventory({ currency }: { currency: CurrencyState }) {
+  const boxes = Array.from({ length: 100 }, (_, index) => index);
+
   return (
-    <div className="h-full w-full bg-gray-800 p-4 rounded-lg ">
+    <div className="h-full w-full bg-gray-800 p-4 rounded-lg flex flex-col items-center">
       <div className="h-1/6">
         <h2 className="text-white ">Gold: {currency.gold}</h2>
+      </div>
+      <div className="grid grid-cols-10 grid-rows-10 w-fit">
+        {boxes.map((index) => (
+          <div
+            key={index}
+            className="w-10 h-10  border border-gray-600  flex items-center justify-center hover:bg-gray-600 transition-colors duration-100"
+          ></div>
+        ))}
       </div>
     </div>
   );
---
+++ src/InventoryItem.tsx
@@ -0,0 +1,7 @@
+export default function InventoryItem() {
+  return (
+    <div className="border-1 flex items-center justify-center h-full w-full bg-gray-700 rounded-lg">
+      <span className="text-white">Dagger</span>
+    </div>
+  );
+}
